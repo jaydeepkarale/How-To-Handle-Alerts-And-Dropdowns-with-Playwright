16adae6bd98a1585499f605969b22ca1201a4a9d
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2025-04-01 04:38:23 +0530

    Update conftest.py to add better fixture to manage cloud run

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,25 @@ import os
 import subprocess
 import urllib
 from dotenv import load_dotenv
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 
 
 load_dotenv()
+
+run_on = os.getenv("RUN_ON") or 'local'
+
+def set_test_status(page: Page, status: str, remark: str):
+    """
+    Function to set stats, title and description of test run on cloud grid
+    :param page: instance of Playwright browser page
+    :param status: status of test e.g Pass or Failed
+    :param remark: additional remark about test status
+    """
+    page.evaluate(
+        "_ => {}",
+        "lambdatest_action: {\"action\": \"setTestStatus\", \"arguments\": {\"status\":\"" + status + "\", \"remark\": \"" + remark + "\"}}"
+    )
+    
 
 capabilities = {
     'browserName': 'Chrome',
@@ -28,19 +43,24 @@ capabilities = {
     }
 }
 
-@pytest.fixture(name="local_grid_page")
-def playwright_local_grid_page():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-        yield page
+@pytest.fixture(name="browser")
+def playwright_browser():     
+    if run_on == 'local':
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            yield browser
+    
+    if run_on == 'cloud':
+        with sync_playwright() as playwright:
+            playwrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
+            capabilities['LT:Options']['playwrightClientVersion'] = playwrightVersion        
+            lt_cdp_url = 'wss://cdp.lambdatest.com/playwright?capabilities=' + urllib.parse.quote(json.dumps(capabilities))
+            browser = playwright.chromium.connect(lt_cdp_url)
+            yield browser   
 
-@pytest.fixture(name="cloud_grid_page")
-def playwright_cloud_grid_page():    
-    with sync_playwright() as playwright:
-        playwrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
-        capabilities['LT:Options']['playwrightClientVersion'] = playwrightVersion        
-        lt_cdp_url = 'wss://cdp.lambdatest.com/playwright?capabilities=' + urllib.parse.quote(json.dumps(capabilities))    
-        browser = playwright.chromium.connect(lt_cdp_url)
-        page = browser.new_page()    
-        yield page 
+
+@pytest.fixture(name="page")
+def page(browser):
+    page = browser.new_context().new_page()        
+    yield page
+    browser.close()
