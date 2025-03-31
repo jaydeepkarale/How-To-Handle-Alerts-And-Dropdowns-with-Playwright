92b726b4f4a2b6ae44ff104d65de3735f8de9f43
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2025-04-01 04:17:27 +0530

    Update test_alerts.py to remove duplicate code

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -12,44 +12,6 @@ from playwright.sync_api import expect, sync_playwright
 
 load_dotenv()
 
-capabilities = {
-    'browserName': 'Chrome',  # Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-    'browserVersion': 'latest',
-    'LT:Options': {
-        'platform': 'Windows 11',
-        'build': 'Playwright Locators Demo Build',
-        'name': 'Playwright Locators Test For Windows 11 & Chrome',
-        'user': os.getenv('LT_USERNAME'),
-        'accessKey': os.getenv('LT_ACCESS_KEY'),
-        'network': False,
-        'video': True,
-        'visual': False,
-        'console': False,
-        'tunnel': False,   # Add tunnel configuration if testing locally hosted webpage
-        'tunnelName': '',  # Optional
-        'geoLocation': '', # country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-    }
-}
-
-
-@pytest.fixture(name="local_grid_page")
-def playwright_local_grid_page():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-        yield page
-
-@pytest.fixture(name="cloud_grid_page")
-def playwright_cloud_grid_page():    
-    with sync_playwright() as playwright:
-        playwrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
-        capabilities['LT:Options']['playwrightClientVersion'] = playwrightVersion        
-        lt_cdp_url = 'wss://cdp.lambdatest.com/playwright?capabilities=' + urllib.parse.quote(json.dumps(capabilities))    
-        browser = playwright.chromium.connect(lt_cdp_url)
-        page = browser.new_page()    
-        yield page
-
-
 # # replace cloud_grid_page with local_grid_page while running on local
 def test_handling_simple_alert(local_grid_page):
     local_grid_page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo")
